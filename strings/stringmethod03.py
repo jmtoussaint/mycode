02807e51376723e573e05389692df5af5495441c
JM Toussaint <jmtoussaint@users.noreply.github.com>
2022-06-14 17:13:17 +0000

    copying local files with python

---
+++ strings/stringmethod03.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Alta3 Research || Author: RZFeeser@alta3.com"""
+
+def main():
+   """ Run-time code"""
+   # create a list of animals
+   lista = ["cat", "dog", "snake", "elephant", "tiger"]
+   print(lista)
+
+   listb = lista.capitalize()
+   print(listb)
+   
+# call our main function
+main()
