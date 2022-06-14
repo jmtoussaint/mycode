c91bb74e865766589618157f93027c7319152a39
JM Toussaint <jmtoussaint@users.noreply.github.com>
2022-06-14 14:36:48 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - simple list"""
+
+def main():
+    # create a list of animal called animal
+    animal = ["Fox", "Fly", "Ant", "Bee", "Cat"]
+
+    # display animal
+    print(animal)
+
+main()
+
